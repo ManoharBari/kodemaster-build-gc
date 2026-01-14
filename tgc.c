45b67c49b6edce9167b340e4e4e39737e28f9636
Manohar Kale <manoharkale5368@gmail.com>
2026-01-14 13:58:23 +0530

    feat: add probe distance function

--- tgc.c
+++ tgc.c
@@ -4,3 +4,9 @@ static size_t tgc_hash(void *ptr) {
     uintptr_t ad = (uintptr_t) ptr;
     return (size_t) ((13*ad) ^ (ad >> 15));
 }
+
+static size_t tgc_probe(tgc_t *gc, size_t i, size_t h) {
+    long v = i - (h-1);
+    if (v < 0) { v = gc->nslots + v; }
+    return v;
+}
